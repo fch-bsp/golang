eca63b5c7e7df929e824a836490105186ab4532e
Fernando Horas <fernando.horas@bspcloud.com>
2022-08-23 12:13:45 -0300

    Update np pacote 1

--- 1-Pacotes/auxiliar/auxiliar.go
+++ 1-Pacotes/auxiliar/auxiliar.go
@@ -1,7 +1,8 @@
 package auxiliar
 
 import "fmt"
-
+// escrever registra escrever
 func Escrever() {
 	fmt.Println("Escrever do pacote auxiliar")
+	escrever2()
 }
--- 1-Pacotes/main.go
+++ 1-Pacotes/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"modulo/auxiliar"
 	"fmt"
+	"github.com/badoux/checkmail"
 
 )	
 
@@ -10,4 +11,7 @@ func main() {
 
 	fmt.Println("Escrevendo modo main")
 	auxiliar.Escrever()
+    // validação de e-mail 
+	erro :=checkmail.ValidateFormat("devbook@gmail.com")
+	fmt.Println(erro)
 }
