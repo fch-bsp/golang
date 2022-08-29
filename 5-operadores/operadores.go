ca71e1ba6e06470831b2ea852dddd9165f836467
Fernando Horas <fernando.horas@bspcloud.com>
2022-08-29 11:39:03 -0300

    update dos dir variveis operadores

--- 5-operadores/operadores.go
+++ 5-operadores/operadores.go
@@ -33,5 +33,62 @@ func main() {
 
 	//OPERADORES RELACIONAIS
 
-	
+	fmt.Println(1 > 2)
+	fmt.Println(1 >= 2)
+	fmt.Println(2 == 2)
+	fmt.Println(1 <= 2)
+	fmt.Println(2 != 2)
+
+	//FIM DOS RELACIONAIS
+
+
+	// OPERADORES LOGICOS
+	verdadeiro, falso := true, false
+
+	fmt.Println(verdadeiro && falso)
+	fmt.Println(verdadeiro || falso)
+	fmt.Println(!verdadeiro)
+	fmt.Println(!falso)
+
+	// FIM DOS OPERADORES LÓGICOS
+
+
+	// OPERADORES UNIÁRIOS
+	numero3 := 10
+	numero3 ++
+	numero3 += 15  // numero = numero + 15
+	fmt.Println(numero3)
+
+	numero4 := 10
+	numero4 -- 
+	numero4 -= 5
+	fmt.Println(numero4)
+
+	numero5 := 10
+	numero5 *= 2
+	fmt.Println(numero5)
+
+	numero6 := 10
+	numero6 /= 2
+	fmt.Println(numero6)
+
+	numero7 := 10
+	numero7 %= 4
+	fmt.Println(numero7)
+	//FIM DOS OPERADORES UNIDARIO
+
+	// OPERADOR TENÁRIO
+
+	var texto string
+	if numero > 5 {
+		texto = "Maior que 5 "
+
+	} else {
+		texto = "Menor que 5 "
+	} 
+
+	fmt.Println(texto)
+
+
+
 }
