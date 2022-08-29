3cae7b40baa641b3b91af2c7c5a88abfb862f260
Fernando Horas <fernando.horas@bspcloud.com>
2022-08-29 10:31:10 -0300

    update dos dir variveis operadores

---
+++ 5-operadores/operadores.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	// ARITIMÉTICOS
+	soma := 1 + 2
+	subtração := 1 - 2
+	divisao := 10 / 4
+	multplicacao := 10 * 5
+	restoDivisao := 10 % 2
+
+	fmt.Println(soma, subtração, divisao, multplicacao, restoDivisao)
+
+	var numero1 int32 = 10
+	var numero2 int32 = 25
+	soma1 := numero1 + numero2
+
+	fmt.Println(soma1)
+
+	// FIM DOS ARITIMÉTICOS
+
+
+	// ATRIBUIÇÃO
+
+	var variavel1 string = "String"
+	variavel2 := "String"
+
+	fmt.Println(variavel1, variavel2)
+
+	// FIM DOS OPERADORES DE ATRIBUIÇÃO
+
+
+	//OPERADORES RELACIONAIS
+
+	
+}
