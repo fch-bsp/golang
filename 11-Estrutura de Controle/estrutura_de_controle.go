c6e46237b670d85525352e1e1c492c74791294a1
Fernando Horas <fernando.horas@bspcloud.com>
2022-09-01 12:03:37 -0300

    update dos dir Estrutura de controle

--- 11-Estrutura de Controle/estrutura_de_controle.go
+++ 11-Estrutura de Controle/estrutura_de_controle.go
@@ -4,11 +4,11 @@ import "fmt"
 
 func main() {
 
-	numero := 20 // criei uma Var que valor de 20
+	numero := 20 // criei uma Var que valor de 20 ok
 
 	if numero >= 15 { // se a var for maior que 15 vc faz isso:
 		fmt.Println("Maior que 15")
-	} else { // caso contrario vc faiz isso
+	} else { // caso contrario vc faiz isso ok
 		fmt.Println("Menor ou igual a 10")
 	}
 
