1e6d93a9d75a6a22eb8a452af0d2096b51b723c4
Fernando Horas <fernando.horas@bspcloud.com>
2022-09-12 09:34:58 -0300

    upgrade dir test

--- 19-Teste Automatizados/1-introducao/enderecos/enderecos_test.go
+++ 19-Teste Automatizados/1-introducao/enderecos/enderecos_test.go
@@ -4,12 +4,16 @@ import "testing"
 
 // Teste de unidade para chamar eh= go test
 
+
+
 type cenariosDeTeste struct {
 	enderecoInserido string
 	retornoEsperado  string
 }
 
-func TestTipoDeEndereco(t *testing.T) {
+func TestTipoDeEndereco(t *testing.T) {  // parametro para executar test
+	t.Parallel() //executa teste em paralelo
+
 	cenariosDeTeste := []cenariosDeTeste{
 		{"Rua Lago das Rosas", "Rua"},
 		{"Rodovia dos Imigrantes", "Rodovia"},
@@ -17,7 +21,7 @@ func TestTipoDeEndereco(t *testing.T) {
 		{"Esquina treze", "Tipo Inválido"},
 		{"Travessa boa menina", "Tipo Inválido"},
 		{"Estrada das coisas", "Estrada"},
-		{"", "Tipo Invalido"},
+		// {"", "Tipo Inválido"},
 		
 	}
 
@@ -31,4 +35,23 @@ func TestTipoDeEndereco(t *testing.T) {
 		}
 	}
 
+}
+
+// executar sem precisar ir até o dir= go test ./... ou go test ou go test -v  ou 
+
+// go test --cover inf % do test
+
+// gerar um relatório TXT comando: go test --coverprofile nome.txt 
+
+// para ler TXT comando go tool cover --func=nome.txt
+
+// para ler TXT comando go tool cover --html=nome.txt
+
+
+
+func TestQaulquer(t *testing.T) {
+	t.Parallel()  //executa teste em paralelo
+	if 1 > 2 {
+		t.Errorf("Teste Quebrou!!")
+	}
 }
